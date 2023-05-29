0bf3bb3394c8e78de5fa68459df6900dbc2c33ac
musnow <ezplayingd@126.com>
2023-05-29 16:13:09 +0800

    更新 上传图片教程中对参数警告进行二次io忽略

--- code/04.img.py
+++ code/04.img.py
@@ -24,18 +24,19 @@ async def img_upload():
     img = None
     with open('./config/logo.png','rb') as f:
         img = io.BytesIO(f.read())
-        # img = io.BytesIO(f.read()).getvalue() # 也可以
+        # 下面的方式也可以，但是在传入create_asset函数时，编译器可能会报参数不匹配的警告
+        # img = io.BytesIO(f.read()).getvalue() 
     
     img_url = await bot.client.create_asset(img) 
     print("open ",img_url)
 
     # 方法3 和PIL库对接
     img = Image.open('./config/logo.png')
-    imgByteArr = io.BytesIO()
-    img.save(imgByteArr, format='PNG') # 保存到内存中
-    # img.save('./test.png') # 保存到磁盘中（测试）
-    imgByte = imgByteArr.getvalue() # 获取到bytes对象
-    img_url = await bot.client.create_asset(imgByte) # 上传
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG') # 保存到内存中
+    # img.save('./test.png') # 保存到磁盘中（仅作测试）
+    img_byte = io.BytesIO(img_byte_arr.getvalue()) # 获取到bytes对象，再套一层io避免报警告
+    img_url = await bot.client.create_asset(img_byte) # 上传
     print("PIL ",img_url)
 
     return img_url
