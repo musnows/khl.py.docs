007cd02850b0c924b3c4b98c4b635be01395fc08
musnow <ezplayingd@126.com>
2023-08-02 19:17:01 +0800

    更新 事件处理的教程和相关代码

--- code/02.channel_send.py
+++ code/02.channel_send.py
@@ -1,5 +1,5 @@
 import traceback
-from khl import Bot, Message
+from khl import Bot, Message, EventTypes, Event
 from utils.file import open_file
 
 # 打开config.json
@@ -13,8 +13,7 @@ bot = Bot(token=config['token'])  # 默认采用 websocket
 async def test_cmd(msg: Message):
     try:
         print("test cmd recv!")
-        ch = await bot.client.fetch_public_channel("3105208745392823"
-                                                   )  # 获取指定频道
+        ch = await bot.client.fetch_public_channel("3105208745392823")  # 获取指定频道
 
         # 使用channel对象的send
         ret = await ch.send("这是一个测试信息，使用了ch.send")  # 方法1
@@ -23,7 +22,21 @@ async def test_cmd(msg: Message):
         # 使用bot对象的client.send
         ret = await bot.client.send(ch, "这是一个测试信息，使用了bot.client.send")  # 方法2
         print(f"bot.client.send | msg_id {ret['msg_id']}")  # 方法2 发送消息的id
-    except:
+    except Exception as result:
+        print(traceback.format_exc())  # 打印报错详细信息
+
+
+@bot.on_event(EventTypes.JOINED_GUILD)
+async def join_guild_send_event(b: Bot, e: Event):
+    try:
+        print("user join guild", e.body)  # 用户加入了服务器
+        ch = await bot.client.fetch_public_channel("3105208745392823")  # 获取指定频道
+
+        ret = await ch.send("用户加入服务器触发了此消息")  # 方法1
+        print(f"ch.send | msg_id {ret['msg_id']}")  # 方法1 发送消息的id
+
+        # 方法2同上
+    except Exception as result:
         print(traceback.format_exc())  # 打印报错详细信息
 
 
